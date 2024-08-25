e86a2c72465a70dac75db7bdd798adc948678ed1
Kayausie <huynhminhkhanh.2004@gmail.com>
2024-08-25 10:17:10 +1000

    second-commit

---
+++ apps/api/src/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(cors())
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript with Express!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
